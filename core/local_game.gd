790f777a8bb3a232a401849403fb79c5d468a30e
Freddy L <neo-ciber_94@hotmail.com>
2025-03-06 21:30:19 -0400

    Add LocalGame class to handle pvp

---
+++ core/local_game.gd
@@ -0,0 +1,107 @@
+class_name LocalGame;
+extends Node
+
+const PLAYER_DEFAULTS = {
+	x = Color.RED,
+	o = Color.BLUE
+}
+
+enum LocalGameMode {
+	LOCAL,
+	CPU,
+}
+
+@onready var _board: Board = $Board;
+@onready var _game_over_message: ResultMessage = $ResultMessage;
+
+@export var mode: LocalGameMode = LocalGameMode.LOCAL;
+@export var difficulty: CpuPlayer.Difficulty = CpuPlayer.Difficulty.RANDOM;
+
+var _players : Dictionary[String, Player] = {}
+var _game_match: Match;
+
+func _ready():
+	_game_over_message.hide();
+	_board.on_hover.connect(_on_hover);
+	
+	start_game()
+
+func start_game():	
+	_setup_players();
+	_start_match()
+	
+func _setup_players():
+	match mode:
+		LocalGameMode.LOCAL:
+			print("starting local match: pvp")			
+			var values = PLAYER_DEFAULTS.keys().duplicate();
+			values.shuffle();
+			
+			_players[values[0]] = HumanPlayer.new(_board);
+			_players[values[1]] = HumanPlayer.new(_board);
+		LocalGameMode.CPU:
+			print("starting local match: pvc")			
+			var values = PLAYER_DEFAULTS.keys().duplicate();
+			values.shuffle();
+			
+			var p1 = values.pop_back();
+			var p2 = values.pop_back();
+			
+			_players[p1] = HumanPlayer.new(_board);
+			_players[p2] = CpuPlayer.new(p2, difficulty)
+	
+func _start_match():
+	_board.prepare_board();
+	_board.fill_slots(Match.EMPTY, Color.TRANSPARENT)
+	
+	_game_match = Match.new();
+	_game_match.add_players(_players);
+	add_child(_game_match);
+	
+	_game_match.on_player_move.connect(_on_player_move)
+	_game_match.on_game_over.connect(_on_game_over)
+	
+	await _board.show_board(true)
+	
+	_game_match.reset_board();
+	_game_match.start_match();
+
+func _on_player_move(player: Player, value: String, index: int):
+	print("_on_player_move: ", { player = player, value = value, index = index })
+	
+	if not value in _players:
+		return;
+	
+	var color = PLAYER_DEFAULTS[value];
+	_board.set_slot_value(index, value, color, true);
+
+func _on_game_over(winner: Winner):
+	var indices = winner.get_indices()
+	_board.show_board(false)
+	await _board.hide_slots(indices);
+	
+	# Show the winner result_message
+	if winner.is_tie():
+		_game_over_message.change_text("It's a tie", Color.BLACK);
+	else:
+		var color = PLAYER_DEFAULTS[winner.get_value()];
+		_game_over_message.change_text("winner!", color)
+		
+	_game_over_message.show();
+
+	# Wait to click for restart
+	await _game_over_message.on_click;
+	_game_over_message.hide();
+	_game_match.queue_free()
+	_start_match();
+	
+func _on_hover(slot: Slot, index: int, is_over: bool):
+	if _game_match.has_value(index):
+		return;
+	
+	if is_over:
+		var current_player = _game_match.get_current_player();
+		var color = PLAYER_DEFAULTS[current_player];
+		slot.set_value(current_player, Color(color, 0.5), false)
+	else:
+		slot.set_value(Match.EMPTY, Color.TRANSPARENT, false)
--- core/match.gd
+++ core/match.gd
@@ -8,7 +8,7 @@ var _board: Array[String] = []
 var _players: Dictionary[String, Player] = {};
 var _current_player: String;
 
-signal on_player_move(value: String, idx: int);
+signal on_player_move(player: Player, value: String, index: int);
 signal on_game_over(winner: Winner);
 signal on_waiting();
 
@@ -27,6 +27,7 @@ func add_players(players: Dictionary[String, Player]) -> void:
 	assert(player_values[0] != player_values[1], "players should be different");
 	
 	_players = players;
+	_current_player = _players.keys().pick_random();
 	
 func start_match() -> void:
 	assert(_players.size() == 2, "expected 2 players to start the match");
@@ -39,13 +40,22 @@ func start_match() -> void:
 			continue;
 		
 		_board[index] = _current_player;
-		on_player_move.emit(_current_player, index);
+		on_player_move.emit(player, _current_player, index);
 		var winner = Utils.check_winner(_board, EMPTY);
 		
 		if winner.is_finished():
 			_declare_winner(winner);
 			break;
+		else:
+			print("switch players")
+			_switch_players();
 	
+func get_current_player():
+	return _current_player;
+	
+func has_value(index: int) -> bool:
+	return _board[index] != EMPTY;
+		
 func _is_valid_move(index: int) -> bool:	
 	if _board[index] == EMPTY:
 		return true;
@@ -69,7 +79,7 @@ func _request_next_move(player: Player) -> int:
 	player.next_move(_board.duplicate())
 	
 	if index.value == -1:
-		print("waiting for play: ", player);
+		print("waiting for play: ", { player = player, current_player = _current_player });
 		await on_waiting;
 		print("wait done: ", player)
 	
@@ -78,10 +88,10 @@ func _request_next_move(player: Player) -> int:
 func _declare_winner(winner: Winner) -> void:
 	on_game_over.emit(winner)
 
-func switch_players() -> void:
+func _switch_players() -> void:
 	_current_player = _get_opponent(_current_player);
 	
 func _get_opponent(value: String) -> String:
 	assert(_players.has(value));
-	var player_values = _players.values();
-	return player_values[0] if value == player_values[1] else player_values[0];
+	var player_values = _players.keys();
+	return player_values[0] if value == player_values[1] else player_values[1];
