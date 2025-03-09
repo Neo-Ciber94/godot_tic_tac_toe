763177a3088aac91edbfcc7e3de0faee469576f5
Freddy L <neo-ciber_94@hotmail.com>
2025-03-08 22:11:32 -0400

    reposition exit button

--- ui/scenes/main_menu.gd
+++ ui/scenes/main_menu.gd
@@ -10,15 +10,20 @@ func _ready():
 	play_online_btn.pressed.connect(_on_play_online)
 
 func _on_play_local():
-	_load_scene(Game.Mode.LOCAL)
+	_load_scene(Constants.GameMode.LOCAL)
 	
 func _on_play_vs_cpu(difficulty: CpuPlayer.Difficulty):
-	_load_scene(Game.Mode.CPU, difficulty)
+	_load_scene(Constants.GameMode.CPU, difficulty)
 	
 func _on_play_online():
-	_load_scene(Game.Mode.ONLINE);
+	_load_scene(Constants.GameMode.ONLINE);
 
-func _load_scene(mode: Game.Mode, difficulty: CpuPlayer.Difficulty = CpuPlayer.Difficulty.RANDOM):
-	GameConfig.game_mode = mode;
-	GameConfig.difficulty = difficulty;
-	get_tree().change_scene_to_file("res://ui/game.tscn")
+func _load_scene(mode: Constants.GameMode, difficulty: CpuPlayer.Difficulty = CpuPlayer.Difficulty.RANDOM):
+	Application.game_mode = mode;
+	Application.difficulty = difficulty;
+	
+	match mode:
+		Constants.GameMode.LOCAL, Constants.GameMode.CPU:
+			get_tree().change_scene_to_file("res://ui/scenes/local_game.tscn")
+		Constants.GameMode.ONLINE:
+			get_tree().change_scene_to_file("res://ui/scenes/online_game.tscn")
